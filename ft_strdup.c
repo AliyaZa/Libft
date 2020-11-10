e501608158837bd970a7125fbc9ab13764deb8f8
Алия Зайнуллина <nhill@ev-p6.kzn.21-school.ru>
2020-11-10 23:58:25 +0300

    Libft

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhill <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/29 13:54:53 by nhill             #+#    #+#             */
+/*   Updated: 2020/11/06 16:58:37 by nhill            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char	*ptr;
+	int		i;
+
+	ptr = NULL;
+	i = 0;
+	if (!(ptr = (char *)malloc(sizeof(*ptr) * (ft_strlen(str) + 1))))
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
